d81dd5c4960f4eb6815e8910ef7c0b85ac223868
Roman Yakovlev <yakovlev.roman@gmail.com>
2014-10-22 13:11:49 +0300

    home view

--- app/home/home.js
+++ app/home/home.js
@@ -10,13 +10,31 @@ angular.module('myApp.home', ['ngRoute'])
 }])
 
 .controller('HomeCtrl', ['$scope','$http',function($scope,$http) {
+
+    var findCategoryByName=function(categoryName,categoriesArray) {
+        for(var i=0;i<categoriesArray.length;i++) {
+            if(categoriesArray[i].name==categoryName) return categoriesArray[i];
+            var category=findCategoryByName(categoryName,categoriesArray[i].children);
+            if(category!=null) return category;
+        }
+
+        return null;
+    }
+
     $http.get('http://localhost:8081/categories.json').success(function(data){
         $scope.categories=data.data;
     });
+
     $http.get('http://localhost:8081/products/lenovo_phones.json').success(function(data){
        $scope.products=data.data;
     });
-    $scope.showProducts=function(categoryName) {
+
+    $scope.toggleCategory=function(categoryName) {
+
+        var category=findCategoryByName(categoryName,$scope.categories);
+
+        category.selected=!category.selected;
+
         $http.get('http://localhost:8081/products/'+categoryName+'.json').success(function(data){
             $scope.products=data.data;
         });
