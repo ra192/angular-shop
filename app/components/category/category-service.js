537d44fb774fedb953b7abe4817ef4255fe5f03f
Roman Yakovlev <yakovlev.roman@gmail.com>
2014-10-28 11:11:02 +0200

    move category service to separate module

---
+++ app/components/category/category-service.js
@@ -0,0 +1,62 @@
+'use strict';
+
+function CategoryService(categoriesArray) {
+
+    var findCategoryByName = function (categoryName, categoriesArray) {
+        for (var i = 0; i < categoriesArray.length; i++) {
+            if (categoriesArray[i].name == categoryName) return categoriesArray[i];
+            var category = findCategoryByName(categoryName, categoriesArray[i].children);
+            if (category != null) return category;
+        }
+
+        return null;
+    };
+
+    var selectCategory = function (categoryName, categoriesArray) {
+        for (var i = 0; i < categoriesArray.length; i++) {
+            if (categoriesArray[i].name == categoryName)
+                categoriesArray[i].selected = true;
+            else
+                categoriesArray[i].selected = false;
+
+            selectCategory(categoryName, categoriesArray[i].children);
+        }
+    };
+
+    this.categoriesArray = categoriesArray;
+
+    this.findCategoryByName = function (categoryName) {
+        return findCategoryByName(categoryName, this.categoriesArray);
+    };
+
+    this.selectCategory = function (categoryName) {
+        selectCategory(categoryName, this.categoriesArray);
+    };
+
+    this.toggleCategory = function (categoryName) {
+
+        var category = this.findCategoryByName(categoryName);
+
+        category.expanded = !category.expanded;
+    };
+}
+
+angular.module('myApp.category', [])
+
+    .factory('categoryService', ['$http', function ($http) {
+
+        var categoryService = new CategoryService([]);
+
+        categoryService.init = function (callback) {
+            if (categoryService.categoriesArray.length == 0) {
+                $http.get('http://localhost:8081/categories.json').success(function (data) {
+                    categoryService.categoriesArray = data.data;
+                    callback();
+                });
+            }
+            else
+                callback();
+        };
+
+        return categoryService;
+    }]);
--- app/home/home.js
+++ app/home/home.js
@@ -1,47 +1,8 @@
 'use strict';
 
-function CategoryService(categoriesArray) {
 
-    var findCategoryByName = function (categoryName, categoriesArray) {
-        for (var i = 0; i < categoriesArray.length; i++) {
-            if (categoriesArray[i].name == categoryName) return categoriesArray[i];
-            var category = findCategoryByName(categoryName, categoriesArray[i].children);
-            if (category != null) return category;
-        }
 
-        return null;
-    };
-
-    var selectCategory = function (categoryName, categoriesArray) {
-        for (var i = 0; i < categoriesArray.length; i++) {
-            if (categoriesArray[i].name == categoryName)
-                categoriesArray[i].selected = true;
-            else
-                categoriesArray[i].selected = false;
-
-            selectCategory(categoryName, categoriesArray[i].children);
-        }
-    };
-
-    this.categoriesArray = categoriesArray;
-
-    this.findCategoryByName = function (categoryName) {
-        return findCategoryByName(categoryName, this.categoriesArray);
-    };
-
-    this.selectCategory = function (categoryName) {
-        selectCategory(categoryName, this.categoriesArray);
-    };
-
-    this.toggleCategory = function (categoryName) {
-
-        var category = this.findCategoryByName(categoryName);
-
-        category.expanded = !category.expanded;
-    };
-}
-
-angular.module('myApp.home', ['ngRoute', 'facebook'])
+angular.module('myApp.home', ['ngRoute', 'myApp.category', 'facebook'])
 
     .config(['$routeProvider', 'FacebookProvider', function ($routeProvider, FacebookProvider) {
         $routeProvider.when('/home', {
@@ -57,23 +18,7 @@ angular.module('myApp.home', ['ngRoute', 'facebook'])
         FacebookProvider.init('131207310315568');
     }])
 
-    .factory('categoryService', ['$http', function ($http) {
 
-        var categoryService = new CategoryService([]);
-
-        categoryService.init = function (callback) {
-            if (categoryService.categoriesArray.length == 0) {
-                $http.get('http://localhost:8081/categories.json').success(function (data) {
-                    categoryService.categoriesArray = data.data;
-                    callback();
-                });
-            }
-            else
-                callback();
-        };
-
-        return categoryService;
-    }])
 
     .controller('HomeCtrl', ['$scope', '$http', '$routeParams', '$location', 'categoryService', 'Facebook',
         function ($scope, $http, $routeParams, $location, categoryService, Facebook) {
